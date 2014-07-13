15c0a0a86e9510a1e37696d427fd5ed3df07ff40
Felix Rech <Felix.Rech@online.de>
2014-07-13 12:50:20 +0200

    Implement calculateMinimal and calculatePartition partially

--- src/com/pseuco/project/LtsMinimizer.java
+++ src/com/pseuco/project/LtsMinimizer.java
@@ -4,19 +4,23 @@ public class LtsMinimizer {
 	public Lts minimize(Lts lts) {
 		Lts weakLts = calculateWeakLts(lts);
 		Partition partition = calculatePartition(weakLts);
-		return calculateMinimal(weakLts, partition);
+		return calculateMinimal(lts, weakLts, partition);
 	}
 	
 	private Lts calculateWeakLts(Lts lts) {
     	throw new UnsupportedOperationException();
 	}
-	
-	private Partition calculatePartition(Lts lts) {
-    	throw new UnsupportedOperationException();
+
+	private Partition calculatePartition(final Lts lts) {
+    	final Bisimulation b = new Bisimulation(lts);
+    	return b.calculateCoarsestPartition();
 	}
 	
-	private Lts calculateMinimal(Lts lts, Partition partition) {
-    	throw new UnsupportedOperationException();
+	private Lts calculateMinimal(final Lts strongLts, final Lts weakLts,
+			final Partition partition) {
+		ObservationalCongruentCondensation c = new ObservationalCongruentCondensation(
+				strongLts, weakLts, partition);
+    	return c.calculate();
 	}
 
 }
