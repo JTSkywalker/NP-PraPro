e15678fdac9209c58900c797a11ae14501ed3ea3
Felix Rech <Felix.Rech@online.de>
2014-07-13 12:32:34 +0200

    Add outTransitions method to Lts

--- src/com/pseuco/project/Lts.java
+++ src/com/pseuco/project/Lts.java
@@ -3,31 +3,37 @@ package com.pseuco.project;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 
 public class Lts {
 
-	private final List<State> states;
-	private final List<Action> actions;
-	private final List<Transition> transitions;
-	private Map<State, Map<Action, Set<State>>> postMap
-			= new HashMap<State, Map<Action, Set<State>>>();
-	private Map<State, Map<Action, Set<State>>> preMap
-			= new HashMap<State, Map<Action, Set<State>>>();
+	private final Collection<State> states;
+	private final Collection<Action> actions;
+	private final Collection<Transition> transitions;
+	private final State initialState;
+	private Map<State, Map<Action, Set<State>>> postMap =
+			new HashMap<State, Map<Action, Set<State>>>();
+	private Map<State, Map<Action, Set<State>>> preMap =
+			new HashMap<State, Map<Action, Set<State>>>();
+	private Map<State, Collection<Transition>> outTransitionsMap =
+			new HashMap<State, Collection<Transition>>();
 
-	public Lts(final List<State> states, final List<Action> actions,
-			final List<Transition> transitions) {
+	public Lts(final Collection<State> states, final Collection<Action> actions,
+			final Collection<Transition> transitions,
+			final State initialState) {
 		this.states = states;
 		this.actions = actions;
 		this.transitions = transitions;
+		this.initialState = initialState;
 		
 		for (State s : states) {
 			Map<Action, Set<State>> outMap = new HashMap<Action, Set<State>>();
 			Map<Action, Set<State>> inMap = new HashMap<Action, Set<State>>();
 			postMap.put(s, outMap);
 			preMap.put(s, inMap);
+			outTransitionsMap.put(s, new LinkedList<Transition>());
 			for (Action a : actions) {
 				outMap.put(a, new HashSet<State>());
 				inMap.put(a, new HashSet<State>());
@@ -36,7 +42,20 @@ public class Lts {
 		for (Transition t : transitions) {
 			postMap.get(t.getSource()).get(t.getLabel()).add(t.getTarget());
 			preMap.get(t.getTarget()).get(t.getLabel()).add(t.getSource());
+			outTransitionsMap.get(t.getSource()).add(t);
 		}
+	}
+
+	public Collection<State> getStates() {
+		return states;
+	}
+	
+	public Collection<Action> getActions() {
+		return actions;
+	}
+	
+	public State getInitialState() {
+		return initialState;
 	}
 
 	public Set<State> post(final State source, final Action a) {
@@ -61,6 +80,10 @@ public class Lts {
 			result.addAll(pre(s, a));
 		}
 		return result;
+	}
+
+	public Collection<Transition> outTransitions(State s) {
+		return outTransitionsMap.get(s);
 	}
 
 	public boolean equals(Object obj) {
