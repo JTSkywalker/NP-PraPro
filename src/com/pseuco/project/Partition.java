3771438d71e3d75604554d9f978bb74a4af21aae
Felix Rech <Felix.Rech@online.de>
2014-07-13 12:44:34 +0200

    Implement Partition

--- src/com/pseuco/project/Partition.java
+++ src/com/pseuco/project/Partition.java
@@ -1,5 +1,31 @@
 package com.pseuco.project;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Partition {
+
+	private final Collection<Collection<State>> blocks;
+	private final Map<State, Collection<State>> stateToBlockMap =
+			new HashMap<State, Collection<State>>();
+
+	public Partition(final Collection<Collection<State>> blocks) {
+		this.blocks = blocks;
+		for (final Collection<State> b : blocks) {
+			for (final State s : b) {
+				stateToBlockMap.put(s, b);
+			}
+		}
+	}
+
+	public Collection<State> getContainingBlock(final State s) {
+		return stateToBlockMap.get(s);
+	}
+
+	@Override
+	public String toString() {
+		return blocks.toString();
+	}
 
 }
