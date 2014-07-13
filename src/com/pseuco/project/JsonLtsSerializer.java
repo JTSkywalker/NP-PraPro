a4183c2f7626dee73c6224ba60d859d4ebc400c7
JTSkywalker <zero.x@gmx.de>
2014-07-13 19:24:50 +0200

    Implement Serializer

--- src/com/pseuco/project/JsonLtsSerializer.java
+++ src/com/pseuco/project/JsonLtsSerializer.java
@@ -1,8 +1,50 @@
 package com.pseuco.project;
 
+import java.io.StringReader;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+
 public class JsonLtsSerializer {
 	public Lts deserialize(String input) {
-		throw new UnsupportedOperationException();
+		Collection<State> states = new LinkedList<>();
+		Collection<Transition> transitions = new LinkedList<>();
+		Collection<Action> actions = new ConcurrentSkipListSet<>();
+		
+		JsonObject ltsObject = Json.createReader(new StringReader(input)).readObject();
+		State initialState = new State(ltsObject.getString("initialState"));
+		JsonObject statesObject = ltsObject.getJsonObject("states");
+		for (String state : statesObject.keySet()) {
+
+			JsonObject stateObject = statesObject.getJsonObject(state);
+
+			JsonArray transitionsArray = stateObject.getJsonArray("transitions");
+
+			for (int i = 0; i < transitionsArray.size(); i++) {
+
+				JsonObject transition = transitionsArray.getJsonObject(i);
+
+				Action action = new Action(transition.getString("label"));
+				String target = transition.getString("target");
+				actions.add(action);
+				
+
+				// TODO decide whether to do something about it, else remove
+				// String detailsLabel = null;
+				// try {
+				// 	detailsLabel = transition.getString("detailsLabel");
+				//} catch (ClassCastException e) {
+					// ignore - detailsLabel = null
+				//}
+
+			}
+
+		}
+		return new Lts(states, actions, transitions, initialState);
 	}
 	
 	public String serialize(Lts output) {
