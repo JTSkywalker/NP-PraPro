b7c6eeb7ef51b1a16a3e0444196cc8974d07d915
Felix Rech <Felix.Rech@online.de>
2014-07-15 13:45:41 +0200

    Implement PairwiseInternalReachabilityChecker

---
+++ src/com/pseuco/project/PairwiseInternalReachabilityChecker.java
@@ -0,0 +1,68 @@
+package com.pseuco.project;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class PairwiseInternalReachabilityChecker {
+
+	private class FixedSourceReachabilityCheck implements Runnable {
+
+		final State source;
+		final Set<State> marked = new HashSet<State>();
+
+		public FixedSourceReachabilityCheck(final State source) {
+			this.source = source;
+		}
+
+		@Override
+		public void run() {
+			explore(source);
+			reachabilityMap.put(source, marked);
+		}
+
+		private void explore(State s) {
+			if (marked.contains(s)) {
+				return;
+			}
+
+			marked.add(s);
+			for (State s2 : lts.post(s, Action.INTERNAL)) {
+				explore(s2);
+			}
+		}
+	}
+
+	private final int NUM_THREADS = Runtime.getRuntime().availableProcessors();
+	private final Lts lts;
+	private final Map<State, Set<State>> reachabilityMap =
+			new ConcurrentHashMap<State, Set<State>>();
+
+	public PairwiseInternalReachabilityChecker(final Lts lts)
+			throws InterruptedException {
+		this.lts = lts;
+		final ExecutorService executor = Executors
+				.newFixedThreadPool(NUM_THREADS);
+		for (State s : lts.getStates()) {
+			executor.execute(new FixedSourceReachabilityCheck(s));
+		}
+		executor.shutdown();
+		while (!executor.isTerminated()) {
+			try {
+				executor.awaitTermination(Long.MAX_VALUE,
+						TimeUnit.MILLISECONDS);
+			} catch (InterruptedException e) {
+				executor.shutdownNow();
+				throw e;
+			}
+		}
+	}
+
+	public boolean isReachable(final State source, final State target) {
+		return reachabilityMap.get(source).contains(target);
+	}
+}
