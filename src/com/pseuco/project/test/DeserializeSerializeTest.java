8c49d63e3ac8ed48c17f5b0be4e6f1ab1bbc2eb2
JTSkywalker <zero.x@gmx.de>
2014-07-16 00:13:55 +0200

    implement: deserialize test

--- src/com/pseuco/project/test/DeserializeSerializeTest.java
+++ src/com/pseuco/project/test/DeserializeSerializeTest.java
@@ -2,13 +2,64 @@ package com.pseuco.project.test;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class DeserializeSerializeTest {
+import com.pseuco.project.*;
 
+public class DeserializeSerializeTest {
+	
+	private static Lts lts;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		Set<State> states = new HashSet<>(3);
+		Set<Action> actions = new HashSet<>(3);
+		Set<Transition> transs = new HashSet<>(3);
+
+		String abStopPlusτStop = "a.b.0 + τ.0";
+		String bStop = "b.0";
+		String stop = "0";
+		String a = "a";
+		String b = "b";
+		String τ = "τ";
+		states.add(new State(abStopPlusτStop));
+		states.add(new State(bStop));
+		states.add(new State(stop));
+		actions.add(new Action(a));
+		actions.add(new Action(b));
+		actions.add(new Action(τ));
+		transs.add(new Transition(new State(abStopPlusτStop),
+                new Action(a),
+                new State(bStop)));
+		transs.add(new Transition(new State(abStopPlusτStop),
+                new Action(τ),
+                new State(stop)));
+		transs.add(new Transition(new State(bStop),
+                new Action(b),
+                new State(stop)));
+		
+		lts = new Lts(states, actions, transs, new State(abStopPlusτStop));
+	}
+	
+	
 	@Test
 	public void testDeserialize() {
-		fail("Not yet implemented");
+		try {
+			FileReader fread = new FileReader(new File("test/testLts"));
+			//String content = scanner.useDelimiter("\\Z").next();
+			//scanner.close();
+			//JsonLtsSerializer jls = new JsonLtsSerializer();
+			//assertEquals(jls.deserialize(content), lts);				
+		} catch (FileNotFoundException e) {
+			fail("test broken (FileNotFoundException)");
+		}
 	}
 
 	@Test
